87dc1048d1573ecbf216595db270ff134d08095c
kevinjhonath28 <133740887+kevinjhonath28@users.noreply.github.com>
2023-06-30 23:56:41 +0800

    Add files via upload

---
+++ KevinJhonatan_2110010212_TugasAkhir/db_siswa/ProjectSiswa.dpr
@@ -0,0 +1,13 @@
+program ProjectSiswa;
+
+uses
+  Forms,
+  Unit1 in '..\..\..\..\Program Files (x86)\Borland\Delphi7\Projects\latihandelphi4n\Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
